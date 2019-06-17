a87c420c80348cacf8900e9141b560bd62c40d39
devlopix <othmane.devlopix@gmail.com>
2019-06-17 19:13:03 +0100

    init project structure

---
+++ src/main/java/com/codechallengeapi/CodeChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.codechallengeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallengeapi/CodeChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallengeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
